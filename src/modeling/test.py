29d5ee1fde5ca4c5623ca642982e2ecee626d9d1
edwardbensa <edwardbensa@gmail.com>
2025-06-22 16:42:46 +0100

    modified test

--- src/modeling/test.py
+++ src/modeling/test.py
@@ -1,17 +1,17 @@
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path(__file__).parent.parent))
-from import_helper import config
-from import_helper import utils
-from utils import load_model, evaluate_model
+
 import pandas as pd
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from import_helper import config, utils
+from utils import load_model, evaluate_model
+
 
 # Load test data
 X_test = pd.read_csv(config.MODEL_DATA_DIR / 'test.csv').iloc[:, :-1]
 y_test = pd.read_csv(config.MODEL_DATA_DIR / 'test.csv').iloc[:, -1]
 target_mapping = {'Low': 0, 'Moderate': 1, 'High': 2}
-
-#t
 
 # Load model
 model_filename = 'train.pkl'
